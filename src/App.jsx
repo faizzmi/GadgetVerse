0b4bb56cb9577deda939730f812151edf4acba8d
Faiz <83203236+faiz@users.noreply.github.com>
2024-12-25 21:35:02 +0800

    highlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
-import Highlight from "./components/Highlight"
+import Highlights from "./components/Highlights"
 
 const App = () => {
 
@@ -8,7 +8,7 @@ const App = () => {
     <main className="bg-black">
       <NavBar/>
       <Hero />
-      <Highlight/>
+      <Highlights/>
     </main>
   )
 }
