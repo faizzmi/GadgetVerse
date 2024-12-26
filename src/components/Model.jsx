41c82629c36d901656f8d349e6efaad49798e199
Faiz <83203236+faiz@users.noreply.github.com>
2024-12-26 16:22:51 +0800

    up

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,12 +2,13 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
 import { yellowImg } from "../utils";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import * as THREE from 'three';
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animation";
 
 const Model = () => {
 
@@ -27,6 +28,24 @@ const Model = () => {
 
     const [smallRotation, setSmallRotation] = useState(0);
     const [largeRotation, setLargeRotation] = useState(0);
+
+    const tl = gsap.timeline();
+
+    useEffect(() => {
+        if(size === 'large'){
+            animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+                transform: 'translateX(-100%)',
+                duration: 2
+            })
+        }
+
+        if(size === 'small'){
+            animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+                transform: 'translateX(0)',
+                duration: 2
+            })
+        }
+    })
 
     useGSAP(() => {
         gsap.to('#heading', {y: 0, opacity: 1})
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,6 +3,9 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import Lights from "./Lights";
 import { Suspense } from "react";
 import Iphone from "./Iphone";
+import Loader from './Loader'
+
+import * as THREE from 'three';
 
 const ModelView = ( 
     { index, groupRef, gsapStyle, controlRef, setRotationSize, size, item}
@@ -11,7 +14,7 @@ const ModelView = (
         <View 
             index={index} 
             id={gsapStyle}
-            className={`border-2 border-red-500 w-full h-full ${index === 2}? 'right-[-100%] : ''`}
+            className={`w-full h-full absolute ${index === 2? 'right-[-100%]' : ''}`}
         
         >
             <ambientLight intensity={0.3} />
@@ -20,9 +23,23 @@ const ModelView = (
 
             <Lights />
 
-            <Suspense fallback={<Html><div>Loading</div></Html>}>
-                <Iphone />
-            </Suspense>
+            <OrbitControls 
+            makeDefault
+            ref={controlRef}
+            enableZoom={false}
+            enablePan={false}
+            rotateSpeed={0.4}
+            target={new THREE.Vector3(0,0,0)}
+            onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+            />
+            <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0,0,0]}>
+                <Suspense fallback={<Html><div><Loader /></div></Html>}>
+                    <Iphone 
+                    scale={ index === 1 ? [15,15,15] : [17,17,17] }
+                    item={item}
+                    size={size}/>
+                </Suspense>
+            </group>
 
         </View>
     )
